5605131557761e94cc9504804b450ee9a0952eed
山根英次 <eijiyamane@gmail.com>
2023-03-19 00:49:14 +0900

    Implementation of parameters2.py (in progress)

--- jasmine_toolkit/utils/parameters2.py
+++ jasmine_toolkit/utils/parameters2.py
@@ -1,19 +1,39 @@
-import threading
+from astropy.constants import Constant
+import pkg_resources
+import yaml
+
+# TODO 規格の切り返したい場合は、このクラスから切り離した方が良いです
+# 規格の切り替えはこの辺で行ってます
+# see https://github.com/astropy/astropy/blob/main/astropy/constants/config.py
+class JasmineConstant(Constant):
+    default_reference = "JASMINE"
+    _registry = {}
+    _has_incompatible_units = set()
 
 class Parameters2:
     __instance = None
     __ignore_list = (
         '__is_dirty',
-        '__check_dirty'
+        '__check_dirty',
+        '__constants',
+        'ready'
     )
     def __new__(cls, *args, **kwargs):
         if Parameters2.__instance is None:
-            Parameters2.__instance = super(Parameters2, cls).__new__(cls)
+            return super(Parameters2, cls).__new__(cls)
         return Parameters2.__instance
 
     def __init__(self):
+        if not Parameters2.__instance is None:
+            return
         self.__is_dirty = True
+        self.__constants = {}
         self.__dummy = 123
+        with open(pkg_resources.resource_filename('jasmine_toolkit', 'utils/constants/constants.yaml')) as file:
+            obj = yaml.safe_load(file)
+            print(obj)
+            for name, val in obj.items():
+        Parameters2.__instance = self
 
     def __setattr__(self, name, value):
         if not name.endswith(Parameters2.__ignore_list):
@@ -45,6 +65,38 @@ class Parameters2:
                 raise RuntimeError('getter don\'t call!!')
             else:
                 raise RuntimeError('setter don\'t call!!')
+
+def _translate_value(dic):
+    description = _extract_description(dic)
+    unit = _extract_unit(dic)
+    value = _extract_value(dic)
+    return
+
+def _extract_description(dic):
+    return __extract_value(dic, 'description')
+
+def _extract_unit(dic):
+    return __extract_value(dic, 'unit')
+
+def _extract_value(dic):
+    val = __extract_value(dic, 'value')
+    if type(val) is bool:
+        return val
+    try:
+        return int(val)
+    except ValueError:
+        pass
+    try:
+        return float(val)
+    except ValueError:
+        pass
+    v = eval(val)
+#    print(v)
+#    print(type(v))
+    return v
+
+def __extract_value(dic, key):
+    return dic[key] if key in dic else ''
 
 
 Parameters2()   # Python 3.7以降なら__new__自体がスレッドセーフなのでいらないらしいです
--- jasmine_toolkit/utils/test/test_parameters2.py
+++ jasmine_toolkit/utils/test/test_parameters2.py
@@ -1,8 +1,24 @@
+import jasmine_toolkit.utils.parameters2 as px
 from jasmine_toolkit.utils.parameters2 import Parameters2
+import pkg_resources
+import yaml
+
+
+#def test_dummy():
+#    filename = pkg_resources.resource_filename('jasmine_toolkit', 'utils/constants/constants.yaml')
+#    print(filename)
+#    print(type(filename))
+#    print(eval("__import__('math').radians(-1.4)"))
+#    with open(filename) as file:
+#        obj = yaml.safe_load(file)
+#        print(obj)
 
 def test_singleton():
+    print("test__sigleton")
+    print("new 1")
     p1 = Parameters2()
     assert p1 is not None
+    print("new 2")
     p2 = Parameters2()
     assert p1 == p2
 
@@ -25,6 +41,31 @@ def test_clean_mode():
     except RuntimeError as e:
         print (e)
     d = p.dummy
+
+def test_get_yaml_data():
+    p = Parameters2()
+    p.ready()
+#    print(p.EARTH_MASS)
+
+def test_extract_description():
+    d1 = {'description':'hoge'}
+    assert 'hoge' == px._extract_description(d1)
+    assert '' == px._extract_description({})
+
+def test_extract_unit():
+    d1 = {'unit':'kg'}
+    assert 'kg' == px._extract_unit(d1)
+    assert '' == px._extract_unit({})
+
+def test_extract_value():
+    assert type(px._extract_value({'value':'100'})) is int
+    assert type(px._extract_value({'value':'5.97'})) is float
+    assert type(px._extract_value({'value':'5.9724E24'})) is float
+    assert type(px._extract_value({'value':False})) is bool
+    assert type(px._extract_value({'value':"__import__('math').radians(-1.4)"})) is float
+    complex_statement = "__import__('jasmine_toolkit.datamodel.efficiency').datamodel.efficiency.Efficiency.from_json(__import__('pkg_resources').resource_filename('jasmine_toolkit', 'data/qe/qe170.json'))"
+    print(type(px._extract_value({'value': complex_statement})))
+    print(px._extract_value({'value': complex_statement}))
 
 def __turn_dirty():
     setattr(Parameters2(), '_Parameters2__is_dirty', True)
