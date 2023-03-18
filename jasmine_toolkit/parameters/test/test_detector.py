d6e9b5e81da4f3b82164564a94ff1961c2789849
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2023-03-18 21:37:03 +0900

    implement `update` method

--- jasmine_toolkit/parameters/test/test_detector.py
+++ jasmine_toolkit/parameters/test/test_detector.py
@@ -7,5 +7,6 @@ def test_dummy():
     print(p.naxis1)
     print(p.naxis2)
     print(p.detector.naxis1)
-    p.naxis1 * 2
+    assert p.naxis1 == p.naxis1
+    assert p.naxis1 == p.detector.naxis1
     assert False
