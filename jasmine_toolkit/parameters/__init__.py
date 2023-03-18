674d263ecec21fff68957573cc41bd7ee0c5a695
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2023-03-18 20:34:07 +0900

    mock implementation of parameters

---
+++ jasmine_toolkit/parameters/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+from .parameter import Parameter, fix_parameters
+from .detector import *
+from .telescope import *
+
+module = sys.modules['jasmine_toolkit.parameters']
+Module = type(module)
+
+
+class ProtectedModule(Module):
+    def __setattr__(self, attr, val):
+        exists = getattr(self, attr, None)
+        if exists is None:
+            if attr == 'test':
+                super().__setattr__(attr, val)
+            else:
+                raise NameError(f'"{attr}" is not defined.')
+        if hasattr(exists, '__assign__'):
+            exists.__assign__(val)
+        super().__setattr__(attr, val)
+
+
+# debug output
+Parameter.list_parameters()
+
+for name in sys.modules.keys():
+    if name.startswith('jasmine_toolkit.parameters'):
+        module = sys.modules[name]
+        module.__class__ = ProtectedModule
---
+++ jasmine_toolkit/parameters/telescope.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from .parameter import Parameter
+
+__all__ = (
+    'effective_focal_length',
+)
+
+effective_focal_length = Parameter(
+    'effective_focal_length', 3776.0, 'mm', 'temp')
