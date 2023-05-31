895841c9f90145019ca812d8bc4a3dcc801c3985
yamada <yamada.yoshiyuki@gmail.com>
2023-05-31 15:47:34 +0900

    Add tests for attityde.py

--- jasmine_toolkit/satellite/attitude/test/test_attitude.py
+++ jasmine_toolkit/satellite/attitude/test/test_attitude.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 from astropy.coordinates import SkyCoord
 import astropy.units as u
@@ -15,3 +16,22 @@ def test_position_angle():
     time = Time('2022-03-21T00:00:00')
     pa = a.get_position_angle(coord, time)
     assert math.isclose(pa, 1.57, abs_tol=0.01)
+
+
+def test_position_angle_over_pi():
+    a: Attitude = Attitude(EnumPointingFreedom.POINTING_FIXED)
+    coord = SkyCoord(lon=90.0 * u.deg, lat=0.0 * u.deg,
+                     frame='barycentricmeanecliptic')
+    time = Time('2022-09-23T00:00:00')
+    pa = a.get_position_angle(coord, time)
+    assert math.isclose(pa, 4.71, abs_tol=0.01)
+
+
+def test_position_angle_over_random(monkeypatch):
+    monkeypatch.setattr(random, 'random', lambda : 1)
+    a: Attitude = Attitude(EnumPointingFreedom.POINTING_RANDOM)
+    coord = SkyCoord(lon=90.0 * u.deg, lat=0.0 * u.deg,
+                     frame='barycentricmeanecliptic')
+    time = Time('2022-02-05T00:00:00')
+    pa = a.get_position_angle(coord, time)
+    assert math.isclose(pa, 2.33, abs_tol=0.005)
