9b4b37f3cab6bae7da2b62931656762e6681b5c2
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2025-05-15 18:46:35 +0900

    fix indentation

--- jasmine_toolkit/bin/toolkit.py
+++ jasmine_toolkit/bin/toolkit.py
@@ -7,11 +7,11 @@ __description__ = '''JASMINE Toolkit command'''
 
 
 def setup_dump_table(parser):
-  from jasmine_toolkit.utils.table import dump_table
-  from jasmine_toolkit.utils.table import setup_parser
+    from jasmine_toolkit.utils.table import dump_table
+    from jasmine_toolkit.utils.table import setup_parser
 
-  setup_parser(parser)
-  parser.set_defaults(handler=dump_table)
+    setup_parser(parser)
+    parser.set_defaults(handler=dump_table)
 
 
 def main():
